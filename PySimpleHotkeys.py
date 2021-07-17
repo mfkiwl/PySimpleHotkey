b55b89c197056f4c1bf33a20163b278d2be60ea6
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2021-07-17 12:36:05 -0400

    Initial check in

---
+++ PySimpleHotkeys.py
@@ -0,0 +1,157 @@
+import PySimpleGUI as sg
+import keyboard
+import psgtray
+
+"""
+    PySimpleHotkey
+    
+    A simple hotkey manager that runs in the system tray.
+    
+    Packages required:
+        PySimpleGUI
+        keyboard
+        psgtray
+    
+    Define your hotkeys in the dictionary.  At the moment, the actions are to insert text or launch a program.
+    
+    Improvement ideas:
+        This program could be extended to store the settings in a User Settings file instead of in the code.
+        The keyboard handler is hooking in a low level. 
+            Callbacks into this code happen when shift, control, etc are pressed
+            There are likely better ways that the keyboard package could be used
+            Didn't take the time to dig further into optimizing
+    
+    Keyboard package and some code borrowed from a project named "pingmote". More info here:
+        https://github.com/dchen327/pingmote
+    It was forked, modified, and a system tray version posted as a PySimpleGUI repo:
+        https://github.com/PySimpleGUI/pingmote
+    
+    Copyright 2021 PySimpleGUI
+"""
+
+
+'''
+MM"""""""`YM          MP""""""`MM oo                     dP          M""MMMMM""MM            dP   dP                         
+MM  mmmmm  M          M  mmmmm..M                        88          M  MMMMM  MM            88   88                         
+M'        .M dP    dP M.      `YM dP 88d8b.d8b. 88d888b. 88 .d8888b. M         `M .d8888b. d8888P 88  .dP  .d8888b. dP    dP 
+MM  MMMMMMMM 88    88 MMMMMMM.  M 88 88'`88'`88 88'  `88 88 88ooood8 M  MMMMM  MM 88'  `88   88   88888"   88ooood8 88    88 
+MM  MMMMMMMM 88.  .88 M. .MMM'  M 88 88  88  88 88.  .88 88 88.  ... M  MMMMM  MM 88.  .88   88   88  `8b. 88.  ... 88.  .88 
+MM  MMMMMMMM `8888P88 Mb.     .dM dP dP  dP  dP 88Y888P' dP `88888P' M  MMMMM  MM `88888P'   dP   dP   `YP `88888P' `8888P88 
+MMMMMMMMMMMM      .88 MMMMMMMMMMM               88                   MMMMMMMMMMMM                                        .88 
+              d8888P                            dP                                                                   d8888P
+'''
+
+# Defintions of the hotkeys
+DOCSTRING_SHORTCUT = 'ctrl+alt+shift+d'
+DOCSTRING_TYPES_SHORTCUT = 'ctrl+alt+shift+f5'
+PYSIMPLEGUI_SHORTCUT = 'alt+shift+p'
+
+# dictionary that maps a hotkey to an action
+hotkey_dict = {
+    DOCSTRING_SHORTCUT :        (lambda :  sg.execute_py_file(r'DocstringTools\AlignDocstrings.py', '--clipboard', cwd='.')),
+    DOCSTRING_TYPES_SHORTCUT :  (lambda :  sg.execute_py_file(r'DocstringTools\AddTypesToDocstring.py', cwd='.')),
+    PYSIMPLEGUI_SHORTCUT:       (lambda: keyboard.write('PySimpleGUI')),
+    }
+
+
+'''
+M""MMMMM""M M#"""""""'M     dP                         dP       
+M  MMMM' .M ##  mmmm. `M    88                         88       
+M       .MM #'        .M    88d888b. .d8888b. .d8888b. 88  .dP  
+M  MMMb. YM M#  MMMb.'YM    88'  `88 88'  `88 88'  `88 88888"   
+M  MMMMb  M M#  MMMM'  M    88    88 88.  .88 88.  .88 88  `8b. 
+M  MMMMM  M M#       .;M    dP    dP `88888P' `88888P' dP   `YP 
+MMMMMMMMMMM M#########M
+'''
+
+def custom_hotkey(event):
+    """ Hook and react to hotkeys with custom handler """
+    try:
+        pressed_keys = [e.name.lower() for e in keyboard._pressed_events.values()]
+    except AttributeError:  # Fn might return as None
+        pressed_keys = []
+
+    for hotkey, func in hotkey_dict.items():
+        pressed = all(key in pressed_keys for key in hotkey.split('+'))
+        if pressed:
+            # print('matched hotkey found', hotkey)
+            func()
+
+
+'''
+M"""""`'"""`YM          oo          
+M  mm.  mm.  M                      
+M  MMM  MMM  M .d8888b. dP 88d888b. 
+M  MMM  MMM  M 88'  `88 88 88'  `88 
+M  MMM  MMM  M 88.  .88 88 88    88 
+M  MMM  MMM  M `88888P8 dP dP    dP 
+MMMMMMMMMMMMMM
+'''
+
+def main():
+    keyboard.hook(custom_hotkey)
+
+    menu = ['', ['Show Window', 'Hide Window', 'Edit Me', '---',  'Change Icon', ['Happy', 'Sad', 'Plain'], 'Exit']]
+
+    layout = [[sg.T('PySimpleHotkey', font='_ 20')],
+              [sg.T('Double clip icon to restore or right click and choose Show Window')],
+              [sg.Multiline(size=(80,15), reroute_stdout=False, reroute_cprint=True, write_only=True, key='-OUT-')],
+              [sg.B('Hide Window'), sg.B('Show Hotkeys'), sg.B('Edit Me'), sg.B('Versions'), sg.Button('Exit')]]
+
+    window = sg.Window('PySimpleHotkey', layout, finalize=True, enable_close_attempted_event=True, right_click_menu=menu, icon=icon)
+
+    tray = psgtray.SystemTray(menu, single_click_events=False, window=window, icon=icon)
+    tray.show_message('PySimpleHotkey', 'PySimpleHotkey Started!')
+
+    sg.cprint(f'Your hotkeys:', c='white on red')
+    for key in hotkey_dict.keys():
+        sg.cprint(key, c='white on green')
+
+    while True:
+        event, values = window.read()
+
+        # IMPORTANT step. It's not required, but convenient. Set event to value from tray
+        # if it's a tray event, change the event variable to be whatever the tray sent
+        if event == tray.key:
+            sg.cprint(f'System Tray Event = ', values[event], c='white on red')
+            event = values[event]       # use the System Tray's event as if was from the window
+
+        if event in (sg.WIN_CLOSED, 'Exit'):
+            break
+
+        sg.cprint(event, values)
+
+        if event in ('Show Window', sg.EVENT_SYSTEM_TRAY_ICON_DOUBLE_CLICKED):
+            window.un_hide()
+            window.bring_to_front()
+        elif event in ('Hide Window', sg.WIN_CLOSE_ATTEMPTED_EVENT):
+            window.hide()
+            tray.show_icon()        # if hiding window, better make sure the icon is visible
+            tray.show_message('Minimized to Tray', 'PySimpleHotkey minimized to system tray\nDouble Click to Restore')
+            # tray.notify('System Tray Item Chosen', f'You chose {event}')
+        elif event == 'Happy':
+            tray.change_icon(sg.EMOJI_BASE64_HAPPY_JOY)
+        elif event == 'Sad':
+            tray.change_icon(sg.EMOJI_BASE64_FRUSTRATED)
+        elif event == 'Plain':
+            tray.change_icon(sg.DEFAULT_BASE64_ICON)
+        elif event == 'Show Hotkeys':
+            sg.cprint(f'Your hotkeys:', c='white on red')
+            for key in hotkey_dict.keys():
+                sg.cprint(key, c='white on green')
+        elif event == 'Edit Me':
+            sg.execute_editor(__file__)
+        elif event == 'Versions':
+            sg.cprint(sg.get_versions(), c='white on purple')
+
+    tray.close()            # optional but without a close, the icon may "linger" until moused over
+    window.close()
+
+    sg.popup_auto_close('Exiting PySimpleHotkey', '(This window auto-closes)')
+
+
+if __name__ == '__main__':
+    sg.theme('dark gray 13')
+
+    icon = b'iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABmJLR0QA/wD/AP+gvaeTAAAFgklEQVRYCe1YXW8UVRh+Z+bMfra0F3jBhTf+CBN+CDdceWdi/ELUBAI1orWImiIaEhRBiUYSY2L8E0bvjUm7YEsp7bYo9Gu3O3POGZ7n7E53uh2UEN1CMnTPfDzn632f834NIsW/goGCgYKBgoGCgYKBgoHHZsD75dffWvMLi1Wb2F2LJHhLhFc8ZH85EIfljuW8gfF8Zet2pU986zUHuUsP4K2/etoTJJ4ceuZgSy01V6q/zzZEG8ORT01TgS8qVDWljRYD4dmeGukpKI7CxJEora0Ya8VaI2++9KJYKDM/Py/f/fSzVCpVDnVt4vjr7r56965MX/rS9Q0Dc5vicuK1l0XHsSwsLMjXP/wo9foIiNeijI4htIYC1g2o1WpSrVZla31DPPyFpRKmd39aayCy00f0/8bS/eMokhJkKZfLbn/lBxJFFicQa0mSxCkwB+brUODPuTnRUMzAvAKrKKesrKyI53ky24C/9PqGgaX73wLzFQjfuHFjRzZtYvG+uPJN8kfjJszISGd7W1pbW1BGS6lckfrIqPi+7xTI6yMrg+P/a+xh+48eGJfnnj2EEwCbNjEQ2gqPq+YJTgRHo7qmY+Ef1CCvbxjYw/b3ELtNbOgDsH9DJ7Yy8dYxCZWCc3QV4mS2Jszn4pVrUq5U5OzESeojq6ur8sH0Z0PDTr3xqoRhKPfu35fzl75yhHcMnDiGYyaIQBRUBYGzcwUlnJS9y9iBA7K1tSHOg4E5x4U/DBOjA9OcRup1CG8gSiI6YhiFIxjbdeL1jQ2pgmXmBDYqxfvS0pKk0Yqn4UP42dnZoWKUhQrwzgYexepIvMlzHyW3FpfFDJQSHBB1OtJutZzGYaksNWjPWLwf2NTpk0IT2tzclDMfTwtJPDg+SidGJoajDtZCVEDB5qoIqwmUC1RICOk7lP3AUtbTkA9bRziFE5NRxnuLXDB56oSUkSw4mI1Zd2r6AkJqWT6dmuQcWYHznp48OzTszIm3hfGfOYgMUi5aQAWmHkexqAgX2jk1oxMHcGQ2DqbDtLY2GVed8HReHt0wMQqfDSpUgPt7qIV0KeyakKUTw0zI+OgIawwDu7dy584dOCpNzEiz2XRJbWaGzjs8bAM2TyVIMluzudKVKdESI4d5rxw7nvy1ti40IbLOxmy6vd0WhlcVllAb1WBvWvYD83qVwKBMtWpdRiolJrLIfQvQhCg8mw8zYikBDSQIlFNuvzBkVYoke/e30mE5HSMdJy4KJXLu3QmUqUwUFuVENzfQdM6c+8SVGVcvfg4z8oS54PjJ0w6bnnpfaqheSQDtM71nn1Msvad9jUZDLn/7PYQTuXzh/K61UWzKh++c2iPPMkz5PcjDxKYjZuI4hpKweUQhlqrscCr3LmNjYy4XcHPf95wpBThWRgJiTHxZJ+tNe6Rbu93+x7Vz5UFVwL3BsEBjRCEcA52DwrA8psApQ7zfXlyE01DJUJaXmzApX2ZmZnawtbW1HZY4no1r8T7YBvE5lO3M8Mbkr83THx8fdwGFMnK9hdu33d4aczx8xntHjh5N2pGBQohLGd6YH5iJE1SqASpTFnIGddMglsbnzNR/fXyctfPmJLojisnAmv5Xf7o7nSYso6SGafmBcgrmYWQ1nfOo97x18rDs2nv64be60/MBjf+iyN8cHwco3Hh0/f48rN/76E956+Rh2RUz/SA2QjGnTBxb6yl/7xlkJz6Bz0i++Bq0KtpuX0tU6QXx8e0LBfGh8wRKu1skmla03ZL1e39fpcje84cPXy+F5SOsUbMngVPqz8y8ZMdkFebC/QnpE/IJ4wNbCmHSrleHA8GPj7z1t3NvhFGT4YfXKO4km2vr1+du3jja7SiuBQMFAwUDBQMFAwUD+8PAA1iNROeLU6xdAAAAAElFTkSuQmCC'
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,185 @@
+
+<p align="center">
+  <img src="https://raw.githubusercontent.com/PySimpleGUI/PySimpleGUI/master/images/for_readme/Logo%20with%20text%20for%20GitHub%20Top.png" alt="Python GUIs for Humans">
+  <h1 align="center">PySimpleHotkey</h1>
+</p>
+
+
 # PySimpleHotkey
 A hotkey manager that runs in the system tray. Uses PySimpleGUI for the GUI and the system tray.
+
+# Packages Used
+
+This project uses these pip installable packages:
+* PySimpleGUI
+* psgtray
+* keyboard
+
+# Mash-up
+
+This project is a mash-up of some PySimpleGUI demo programs and a program called [pingmote](https://github.com/dchen327/pingmote).  The `pingmote` project is the origin of the keyboard handler.  A big thank you to @dchen327 for the code and inspiration.  
+
+The system tray code is from the [psgtray](https://github.com/PySimpleGUI/psgtray) demo program.  
+
+The 2 utilities that are in the folder DocstringTools are the reason this little project was started.  These tools use the clipboard as input and write back onto the clipboard the results.
+
+
+# Docstring Utilities
+
+The reason that a hotkey was needed in the first place was a conveinent way to launch a couple of simple utilities that operate on the clipboard.  They both read the clipboard, look for a doctring, modify the docstring, and save the result back onto the clipboard.
+
+The idea is to have a workflow that follows these steps:
+
+* Text with docstring is copied onto clipboard
+* Hotkey is pressed to invoke one of the docstring tools
+* The modified clipboard is pasted into the code
+
+## AlignDocstrings.py
+
+This program reads a docstring and lines up the description and types.  It assumed the format of the docstrings is the one used by the PySimpleGUI project.
+
+An example is the easiest way to show what it does.
+
+## Input
+
+This is the input that is on the clipboard:
+
+```python
+def execute_py_file(pyfile, parms=None, cwd=None, interpreter_command=None, wait=False, pipe_output=False):
+    """
+    Executes a Python file.
+    The interpreter to use is chosen based on this priority order:
+        1. interpreter_command paramter
+        2. global setting "-python command-"
+        3. the interpreter running running PySimpleGUI
+
+    :param pyfile: the file to run
+    :type pyfile: (str)
+    :param parms: parameters to pass on the command line
+    :type parms: (str)
+    :param cwd: the working directory to use
+    :type cwd: (str)
+    :param interpreter_command: the command used to invoke the Python interpreter
+    :type interpreter_command: (str)
+    :param wait: the working directory to use
+    :type wait: (bool)
+    :param pipe_output: If True then output from the subprocess will be piped. You MUST empty the pipe by calling execute_get_results or your subprocess will block until no longer full
+    :type pipe_output: (bool)
+    :return: Popen object
+    :rtype: (subprocess.Popen) | None
+    """
+```
+
+
+ 
+
+## Output
+
+
+This is the result that is left on the clipboard:
+
+```python
+def execute_py_file(pyfile, parms=None, cwd=None, interpreter_command=None, wait=False, pipe_output=False):
+    """
+    Executes a Python file.
+    The interpreter to use is chosen based on this priority order:
+        1. interpreter_command paramter
+        2. global setting "-python command-"
+        3. the interpreter running running PySimpleGUI
+        
+    :param pyfile:              the file to run
+    :type pyfile:               (str)
+    :param parms:               parameters to pass on the command line
+    :type parms:                (str)
+    :param cwd:                 the working directory to use
+    :type cwd:                  (str)
+    :param interpreter_command: the command used to invoke the Python interpreter
+    :type interpreter_command:  (str)
+    :param wait:                the working directory to use
+    :type wait:                 (bool)
+    :param pipe_output:         If True then output from the subprocess will be piped. You MUST empty the pipe by calling execute_get_results or your subprocess will block until no longer full
+    :type pipe_output:          (bool)
+    :return:                    Popen object
+    :rtype:                     (subprocess.Popen) | None
+    """
+```
+
+
+## AddTypesToDocstring.py
+
+PyCharm will automatically create a docstring for you. After a function definition, if you enter `"""` then it will populate the docstring using the parameters found in the function definition.  It's a nice feature.
+
+But.... the format of these docstrings is not the 2-line format used by the PySimpleGUI documentation creation tools (call reference in particular).  This utility simply adds the `type` and `rtype` lines in the docstring.  Like the other docstring tool, it takes the clipboard as input and places the result onto the clipboard.
+
+## Input
+
+```python
+def execute_py_file(pyfile, parms=None, cwd=None, interpreter_command=None, wait=False, pipe_output=False):
+    """
+    
+    :param pyfile: 
+    :param parms: 
+    :param cwd: 
+    :param interpreter_command: 
+    :param wait: 
+    :param pipe_output: 
+    :return: 
+    """
+```
+
+
+## Output
+
+This utility only leaves the docstring on the clipboard.  Any other text before or after is stripped off.  As you can see, the difference is that each `param` line has a mataching `type` line and the `return` has a matching `rtype`.
+
+```python
+    """
+    
+    :param pyfile: 
+    :type pyfile:
+    :param parms: 
+    :type parms:
+    :param cwd: 
+    :type cwd:
+    :param interpreter_command: 
+    :type interpreter_command:
+    :param wait: 
+    :type wait:
+    :param pipe_output: 
+    :type pipe_output:
+    :return: 
+    :rtype:
+    """
+```
+
+# Windows Only (most likely)
+
+`psgtray` runs the best on Windows and is used in this project. It's not been tried on Linux nor the Mac.
+
+# Opportunities for Improvement
+
+Not a lot of time was spent on the entire project.  It was some personal utilities that I wrote for myself and then reaslized that maybe others would want to extend their IDEs as well or do other hotkey kind of operations.  
+
+In this project, they keyboard handler hooks in at a low level such that all keystrokes will be passed to the program.  The `keyboard` package likely has a more efficient and higher level interface that's not being used.
+
+"Satisfice" is a word I recently discovered and realized it matches the prototype code that I write. It's pretty hacky kind  of code as it's meant to do a specific job, on my system, and not designed in a high general purpose way.  It's the "first pass" code... raw, simple, working stuff but not much more than that. It should at least work on your system though, assuming you're able to run psgtray and PySimpleGUI. 
+
+I hope you are able to find something here that helps you create your own tools that makes you more efficient.
+
+
+## Designed and written by
+
+mike from PySimpleGUI.org
+
+## Contributing
+
+Like the PySimpleGUI project, the psgtray project is currently licensed under an open-source license, the project itself is structured like a proprietary product. Pull Requests are not accepted.
+
+## License
+
+GNU Lesser General Public License (LGPL 3) +
+
+## Copyright
+
+Copyright 2021 PySimpleGUI
+
